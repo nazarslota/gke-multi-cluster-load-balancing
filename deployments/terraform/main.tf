ed8965b2923e096f96c19f5a755e716f8c961926
nazarslota <nazarslota03@gmail.com>
2023-10-17 17:16:57 +0300

    fix(deployments/terraform): fix docker login and push commands
    The docker login and push commands in the main.tf file were updated to use the correct artifact registry location.

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -70,14 +70,16 @@ resource "null_resource" "docker_build_and_push" {
     command = <<EOL
       cd ../../
 
+      docker login -u _json_key --password-stdin https://${google_artifact_registry_repository.default.location}-docker.pkg.dev < $GOOGLE_APPLICATION_CREDENTIALS
+
       docker build \
         --file deployments/docker/Dockerfile \
-        --tag "us-east4-docker.pkg.dev/${var.project}/${var.application}/${var.application}:${var.build_number}" \
-        --tag "us-east4-docker.pkg.dev/${var.project}/${var.application}/${var.application}:latest" \
-      .
+        --tag "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${var.project}/${var.application}/${var.application}:${var.build_number}" \
+        --tag "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${var.project}/${var.application}/${var.application}:latest" \
+        .
 
-      docker push "us-east4-docker.pkg.dev/${var.project}/${var.application}/${var.application}:${var.build_number}"
-      docker push "us-east4-docker.pkg.dev/${var.project}/${var.application}/${var.application}:latest"
+      docker push "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${var.project}/${var.application}/${var.application}:${var.build_number}"
+      docker push "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${var.project}/${var.application}/${var.application}:latest"
     EOL
   }
 
